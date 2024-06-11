f290a9bdb55e54b3121e5d831af1440836dc4338
Dana Goosen <klymentovska@gmail.com>
2024-06-11 14:37:35 +0200

    Added myTake implementation

---
+++ unit_1/lesson_7_rules_for_recursion_and_pattern_matching/4_my_take.hs
@@ -0,0 +1,6 @@
+myTake _ [] = []
+myTake n myList = myTakeHelper n myList []
+
+myTakeHelper _ [] accumulator = accumulator
+myTakeHelper 0 _ accumulator = accumulator
+myTakeHelper n (x:xs) accumulator = myTakeHelper (n - 1) xs (accumulator ++ [x])
